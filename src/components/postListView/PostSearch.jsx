43779d711adb39465f63264879f597eeb0a82f54
test1 <cooky122@naver.com>
2025-05-29 23:29:03 +0900

    2025-05-29 2328, 게시물 검색기능  완성(검색범위 선택만)

--- src/components/postListView/PostSearch.jsx
+++ src/components/postListView/PostSearch.jsx
@@ -1,26 +1,28 @@
 
 import '../../Css/PostList/PostSearch.css'
 
-const PostSearch = ({duration, type, searchDuration, searchType, setKeyword})=>{
+const PostSearch = ({duration, type, changeDuration, changeType, setKeyword})=>{
   const handleKeyword = (e)=>{
-    setKeyword(e.target.value);
+     e.preventDefault();
+    console.log("keword: " + e.target.value)
+    setKeyword(e.target.keyword.value);
   }
   
   return (
     <div className="PostSearch">
-      <select name="duration" value={duration} onChange={searchDuration}>
+      <select name="duration" value={duration} onChange={changeDuration}>
         <option value={"day"}>1일</option>
         <option value={"month"}>1달</option>
         <option value={"year"}>1년</option>
       </select>
-      <select name="searchType" value={type} onChange={searchType}>
+      <select name="searchType" value={type} onChange={changeType}>
         <option value={"post_title"}>제목</option>
         <option value={"content"}>내용</option>
         <option value={"mem_id"}>글작성자</option>
       </select>
       <form onSubmit={handleKeyword}>
-        <input type="text" name="keyword"></input>
-        <input type="submit" value="검색"></input>
+        <input type="text" name="keyword"/>
+        <input type="submit" value="검색"/>
       </form>
     </div>
   );
